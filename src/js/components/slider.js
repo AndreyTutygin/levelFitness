b90a32ec976523b49292ea6561ad89a344dc6824
andreytutygin <andr3qaa@gmail.com>
2022-11-13 21:20:19 +0530

    Added a hero mobile versions and full version sections benefits, programs

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,12 +4,17 @@ import Swiper, {
 } from 'swiper';
 
 const swiper = new Swiper(".hero-slider__swiper", {
-  modules: [Navigation],
+  modules: [Navigation, Pagination],
   navigation: {
     nextEl: ".btn-arrow_next",
     prevEl: ".btn-arrow_prev",
   },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   grabCursor: true,
   speed: 800,
+  spaceBetween: 100,
   loop: true
 });
